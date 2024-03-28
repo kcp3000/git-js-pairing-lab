8ef4069819f5ec7f20978d7ee77c05291d06eb13
Kevin <mrkev63@gmail.com>
2024-03-28 14:20:22 -0400

    finished q7

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const fiveToOneHundred = () => {
 
     for (let i = 5; i < 101; i++) {
         console.log(`Counting: ${i}`);
-    }
+    };
 
 };
-
 //console.log(fiveToOneHundred())
 
 
@@ -18,7 +17,7 @@ const fiveToOneHundred = () => {
 const multiplesOfThree = () => {
     for (let i = 3; i < 100; i += 3) {
         console.log(i);
-    }
+    };
 };
 //multiplesOfThree()
 
@@ -32,9 +31,8 @@ const multiplesOfThreeOrFive = () => {
     for (let j = 1; j <= 20; j++) {
         const multiple5 = j * 5
         console.log(`${multiple5}`)
-    }
-}
-
+    };
+};
 //console.log(multiplesOfThreeOrFive())
 
 //4.
@@ -42,6 +40,38 @@ const multiplesOfThreeOrFive = () => {
 const untilNum = (num) => {
     for (let i = 1; i <= num; i++) {
         console.log(i);
-    }
+    };
 };
 //untilNum(6)
+
+//5.
+
+const multiply = (num, num2) => {
+    return num * num2
+};
+//console.log(multiply(-2, 2))
+
+//6.
+
+const add = (num1, num2) => {
+    if (num1 === num2) {
+        return console.log(num1 * num2);
+    } else {
+        return console.log(num1 + num2);
+    };
+};
+// add(4, 4)
+
+//7
+
+const isNegative = (num) => {
+    if (num < 0) {
+        return true
+    } else {
+        return false
+    };
+}
+
+console.log(isNegative(1))
+
+//8
