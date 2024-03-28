0e8c3740d30b778b5cd8125f217e25beab8ede8e
Kevin <mrkev63@gmail.com>
2024-03-28 11:56:38 -0400

    Finished question 1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Code your solutions in this file
+
+//Question 1
+
+const fiveToOneHundred = () => {
+
+    for (let i = 5; i < 101; i++) {
+        console.log(`Counting: ${i}`);
+    }
+
+};
+
+//console.log(fiveToOneHundred())
