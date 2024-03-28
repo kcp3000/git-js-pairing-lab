dc5e458e9cb6263f78be50c0cef7b9285fa94676
Kevin <mrkev63@gmail.com>
2024-03-28 12:52:46 -0400

    Question 3 almost done (need corrections)

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ const fiveToOneHundred = () => {
 };
 
 //console.log(fiveToOneHundred())
+
+//Question 3
+
+const multiplesOfThreeOrFive = () => {
+    for (let i = 1; i <= 33; i++) {
+        const multiple3 = i * 3
+        console.log(`${multiple3}`)
+    }
+    for (let j = 1; j <= 20; j++) {
+        const multiple5 = j * 5
+        console.log(`${multiple5}`)
+    }
+}
+
+//console.log(multiplesOfThreeOrFive())
